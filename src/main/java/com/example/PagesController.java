42ce9472c22186dfea32e60f45d26b19c9d779b9
John Slota <john.slota@gmail.com>
2017-02-19 18:01:34 -0700

    add patching and deleting

--- src/main/java/com/example/PagesController.java
+++ src/main/java/com/example/PagesController.java
@@ -1,13 +1,37 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+/**
+ * Created by johnslota on 2/19/17.
+ */
 @RestController
 public class PagesController {
 
-  @GetMapping("/hello")
-  public String hello(){
-    return "Hello World";
-  }
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @PostMapping("/tasks")
+    public String createTask(){
+        return "You're a poster";
+    }
+
+    @PatchMapping("/pathTask")
+    public String patchTask(){
+        return "Patch";
+    }
+
+    @DeleteMapping("/deleteTask")
+    public String deleteTask(){
+        return "delete";
+    }
 }
---
+++ src/test/java/com/example/PagesControllerTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by johnslota on 2/19/17.
+ */
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PagesController.class)
+
+
+public class PagesControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testHomepage() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/hello");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+
+    }
+}
